6b150aac8e118d3fa9c7adc087de65ad265d7101
Ahmed Medra <ahmed.medrah@gmail.com>
2020-01-31 14:44:51 +0200

    initial commit

--- app/src/main/java/com/example/medkit/activities/MainActivity.java
+++ app/src/main/java/com/example/medkit/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.medkit;
+package com.example.medkit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.medkit.R;
 
 public class MainActivity extends AppCompatActivity {
 
