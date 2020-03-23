a49a7151d30c5f34adda80ccbbb7517f02e42d20
Ahmed Medra <ahmed.medrah@gmail.com>
2020-03-23 22:09:47 +0200

    initial notification design and code

---
+++ app/src/main/java/com/example/medkit/model/NotificationModel.java
@@ -0,0 +1,49 @@
+package com.example.medkit.model;
+
+import android.graphics.Bitmap;
+
+public class NotificationModel {
+    private String userName;
+    private Bitmap userPhoto;
+    private String notificationContent;
+    private boolean isRead;
+
+    public NotificationModel(String userName, Bitmap userPhoto, String notificationContent, boolean isRead) {
+        this.userName = userName;
+        this.userPhoto = userPhoto;
+        this.notificationContent = notificationContent;
+        this.isRead = isRead;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Bitmap getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(Bitmap userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    public String getNotificationContent() {
+        return notificationContent;
+    }
+
+    public void setNotificationContent(String notificationContent) {
+        this.notificationContent = notificationContent;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+}
